3c19113d1614c0a378515c6b0a762b8125739ad3
ABHISHEK K <abishkpk@gmail.com>
2024-06-22 17:48:45 +0530

    try again

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
     // 5. Replace the clientID variable below with your actual Client ID obtained from Spotify Developer Dashboard
 
     const clientID = '9407936e742142e6a20e4d272379e015';                                               // Spotify application client ID
-    const redirectURL = 'https://spotify-clone-523.netlify.app';                      // Redirect URL after authorization
+    const redirectURL = 'https://spotify-clone-523.netlify.app/';                      // Redirect URL after authorization
     const apiURL = 'https://accounts.spotify.com/authorize';           // Spotify authorization endpoint
 
     // List of permissions the application is requesting
